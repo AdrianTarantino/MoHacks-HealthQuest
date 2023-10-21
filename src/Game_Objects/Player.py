adf2c10de6793d6a08ceb1ae50c0e25ae13a2eed
AdrianTheHacker <adriantarantino2006@gmail.com>
2023-10-21 10:23:02 -0400

    Writing boilerplate and implementing movement code

---
+++ src/Game_Objects/Player.py
@@ -0,0 +1,40 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       self.velocity = 5
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rect.x -= self.velocity
+        if keys[pygame.K_d]:
+            self.rect.x += self.velocity
+        if keys[pygame.K_w]:
+            self.rect.y -= self.velocity
+        if keys[pygame.K_s]:
+            self.rect.y += self.velocity
+
+    def update(self):
+        super().update(self)
+
+        self.movement()
+
+    def draw(self, screen):
+        self.update()
+        screen.blit(self.image, self.rect)
---
+++ src/main.py
@@ -0,0 +1,34 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+from Game_Objects.Player import Player
+
+WIDTH = 800
+HEIGHT = 600
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+player = Player("white", 50, 50)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+    player.draw(screen)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
